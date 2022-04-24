b7ab679ed4d0fe041494a1050e8ad9f57af3318a
Andrew <atklenner@gmail.com>
2022-04-24 14:37:37 -0700

    Start basic layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,25 @@
+import { Container, AppBar, Typography, Grid } from "@mui/material";
+import Posts from "./components/Posts";
+import Form from "./components/Form";
+
 function App() {
-  return <div></div>;
+  return (
+    <Container maxWidth="lg">
+      <AppBar position="relative">
+        <Typography component="h1" variant="h1">
+          Memories
+        </Typography>
+      </AppBar>
+      <Grid container>
+        <Grid item xs={12} sm={7}>
+          <Posts />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Form />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default App;
