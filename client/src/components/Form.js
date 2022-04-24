51ef03d0af2206458e89f637a0ec5adb3ec0bd74
Andrew <atklenner@gmail.com>
2022-04-24 14:43:06 -0700

    Created form

---
+++ client/src/components/Form.js
@@ -0,0 +1,89 @@
+import { Button, Paper, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+
+export default function Form() {
+  const emptyPost = {
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  };
+
+  const [postData, setPostData] = useState(emptyPost);
+
+  function handleChange(e) {
+    setPostData((prevData) => {
+      return { ...prevData, [e.target.name]: e.target.value };
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log("submitted");
+  }
+
+  return (
+    <Paper component="form" elevation={4} onSubmit={handleSubmit}>
+      <Typography>Create a New Memory</Typography>
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        id="creator"
+        label="Creator"
+        name="creator"
+        type="text"
+        value={postData.creator}
+        onChange={handleChange}
+      />
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        name="title"
+        label="Title"
+        type="text"
+        id="title"
+        value={postData.title}
+        onChange={handleChange}
+      />
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        name="message"
+        label="Message"
+        type="text"
+        id="message"
+        multiline
+        rows={4}
+        value={postData.message}
+        onChange={handleChange}
+      />
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        name="tags"
+        label="Tags (comma separated)"
+        value={postData.tags}
+        onChange={(e) =>
+          setPostData({ ...postData, tags: e.target.value.split(",") })
+        }
+      />
+      <Button variant="contained" fullWidth type="submit">
+        Submit
+      </Button>
+      <Button
+        variant="contained"
+        color="error"
+        fullWidth
+        onClick={() => setPostData(emptyPost)}
+      >
+        Clear
+      </Button>
+    </Paper>
+  );
+}
