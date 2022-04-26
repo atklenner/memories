be5b48c2b1bcfee255d9f2b17cce2ba45650ad61
Andrew <atklenner@gmail.com>
2022-04-26 00:13:44 -0700

    Add import

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const postRouter = require("./routes/postRoutes");
+const userRouter = require("./routes/userRoutes");
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
